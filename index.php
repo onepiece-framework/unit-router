a5b2c70508f51d3aba8d7bd56d06e701d2f2da72
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-09-18 16:16:19 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-router:/index.php
+ *
+ * @creation  2019-02-23
+ * @version   1.0
+ * @package   unit-router
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/Router.class.php');
